34b6f9c5c0f2fa8c80b95df686cd3d84bd6ec197
Nikitha1099 <nikithapyata99@gmail.com>
2020-06-18 22:47:44 +0530

    First Commit

---
+++ java-8/src/main/java/com/java/PalindromeTest.java
@@ -0,0 +1,31 @@
+package com.java;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+class PalindromeTest {
+
+    public static void main(String[] args) {
+        String[] names = {"abc", "aba","aaa"};
+        PossibleCombination possibleCombination = new PossibleCombination(
+                names, CombinationOption.EXCLUDE_DUPLICATED
+        );
+        Stream<String> allCombination = possibleCombination.getAll();
+        allCombination.forEach((String concatName) -> {
+            StringBuilder sb = new StringBuilder(concatName);
+            boolean equal = sb.reverse()
+                    .toString()
+                    .equalsIgnoreCase(concatName);
+            if (equal) {
+                System.out.println(concatName);
+            }
+        });
+    }
+}
+
+
+ 
+
